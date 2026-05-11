e133ff5d2bfa795a4c975f66951edbb71b50ba3a
Volkan Şahin <volkansahin499@gmail.com>
2026-05-11 13:19:27 +0300

    Switch to HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
@@ -9,7 +9,7 @@ if (saved) document.documentElement.setAttribute('data-theme', saved)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
